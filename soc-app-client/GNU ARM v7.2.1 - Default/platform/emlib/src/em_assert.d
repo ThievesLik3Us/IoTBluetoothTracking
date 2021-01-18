44d811058a0b07ddb28f6a3f0005d57d23d3785b
kolby <bluecate.llc@gmail.com>
2021-01-18 11:45:51 -0500

    Added app-client SiLabs Project

---
+++ soc-app-client/GNU ARM v7.2.1 - Default/platform/emlib/src/em_assert.d
@@ -0,0 +1,4 @@
+platform/emlib/src/em_assert.o: ../platform/emlib/src/em_assert.c \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_assert.h
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_assert.h:
---
+++ soc-app-client/GNU ARM v7.2.1 - Default/platform/emlib/src/em_cryotimer.d
@@ -0,0 +1,178 @@
+platform/emlib/src/em_cryotimer.o: ../platform/emlib/src/em_cryotimer.c \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_cryotimer.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/em_device.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22c224f512im40.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/core_cm33.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/cmsis_version.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/cmsis_compiler.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/cmsis_gcc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/mpu_armv8.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/system_efr32bg22.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_emu.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cmu.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfxo.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfrco.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_fsrco.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dpll.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfxo.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfrco.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ulfrco.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_msc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_icache.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio_port.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldma.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_timer.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_usart.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_burtc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_i2c.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_syscfg.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_buram.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpcrc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dcdc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_pdm.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rfsense.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_aes.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_smu.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rtcc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_letimer.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_iadc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_wdog.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_amuxcp.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_eusart.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cryptoacc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_frc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_agc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rfcrc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_modem.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_synth.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_protimer.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rac.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_mailbox.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_bufc.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_devinfo.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs_signals.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dma_descriptor.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar_defines.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_bus.h \
+ C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_bus.h
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_cryotimer.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/em_device.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22c224f512im40.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/core_cm33.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/cmsis_version.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/cmsis_compiler.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/cmsis_gcc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\CMSIS\Include/mpu_armv8.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/system_efr32bg22.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_emu.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cmu.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfxo.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_hfrco.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_fsrco.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dpll.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfxo.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_lfrco.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ulfrco.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_msc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_icache.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpio_port.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldma.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_timer.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_usart.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_burtc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_i2c.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_syscfg.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_buram.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_gpcrc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dcdc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_pdm.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rfsense.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_aes.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_smu.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rtcc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_letimer.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_iadc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_wdog.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_amuxcp.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_eusart.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_cryptoacc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_frc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_agc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rfcrc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_modem.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_synth.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_protimer.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_rac.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_mailbox.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_bufc.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_devinfo.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_prs_signals.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_dma_descriptor.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\Device\SiliconLabs\EFR32BG22\Include/efr32bg22_ldmaxbar_defines.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_bus.h:
+
+C:\Users\User\SimplicityStudio\v4_workspace\soc-app-client\platform\emlib\inc/em_bus.h:
